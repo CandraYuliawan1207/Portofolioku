e195ef3a692b78001fd8589e82d2dd80e31acc80
Candra Yuliawan <candrayln275@gmail.com>
2025-07-11 11:16:00 +0700

    merubah style pada index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include 'includes/header.php'; ?>
 <main>
-    <section class="hero text-black text-center d-flex align-items-center justify-content-center" style="height: 100vh;">
+    <section class="hero text-black text-center d-flex align-items-center justify-content-center" style="height: 80vh;">
         <div data-aos="fade-up">
             <h1 class="display-4 fw-bold">Candra Yuliawan</h1>
             <p class="lead">Office Administration | Web Developer Enthusiast</p>
