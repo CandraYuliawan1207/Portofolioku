53147848ed608100c9ba353062b3915c24cdeba9
Candra Yuliawan <candrayln275@gmail.com>
2025-07-11 22:34:58 +0700

    fixed style overflow-x

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php include 'includes/header.php'; ?>
-<main style="margin-bottom: 0; overflow-y: hidden;">
+<main style="margin-bottom: 0;">
     <section class="hero text-black text-center d-flex align-items-center justify-content-center"
-        style="height: 80vh; position: relative;">
+        style="height: 100vh; position: relative;">
         <!-- Animasi Wave -->
         <img src="https://threedio-prod-var-cdn.icons8.com/if/preview_sets/previews/WdouIOZIHd9p4jHt.webp"
             alt="Waving silhouette man"
-            style="position: absolute; top: 8%; left: 50%; transform: translateX(-50%); width: 500px; height: auto;"
+            style="position: absolute; top: 8%; left: 50%; transform: translateX(-50%); max-width: 100vw; width: 40%; height: auto;"
             data-aos="fade-down" data-aos-duration="1000" />
+
         <div data-aos="fade-up" data-aos-delay="500">
             <h1 class="display-4 fw-bold">Candra Yuliawan</h1>
             <p class="lead">Office Administrator | Web Developer Enthusiast</p>
