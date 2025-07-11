ad02763d52fd49d04aabab76fa81db8e2ddd15c9
Candra Yuliawan <candrayln275@gmail.com>
2025-07-11 14:35:11 +0700

    menambahkan hero charackter pad index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <main style="margin-bottom: 0;">
     <section class="hero text-black text-center d-flex align-items-center justify-content-center" style="height: 80vh; position: relative;">
         <!-- Animasi Wave -->
-        <img src="https://threedio-prod-var-cdn.icons8.com/vb/preview_sets/previews/JpZQHPMv0qeSmJHp.webp"
+        <img src="https://threedio-prod-var-cdn.icons8.com/if/preview_sets/previews/WdouIOZIHd9p4jHt.webp"
             alt="Waving silhouette man"
-            style="position: absolute; top: 12%; left: 50%; transform: translateX(-50%); width: 150px; height: auto;"
+            style="position: absolute; top: 8%; left: 50%; transform: translateX(-50%); width: 500px; height: auto;"
             data-aos="fade-down" data-aos-duration="1000" />
         <div data-aos="fade-up" data-aos-delay="500">
             <h1 class="display-4 fw-bold">Candra Yuliawan</h1>
